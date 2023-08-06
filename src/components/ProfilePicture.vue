7c034ce198ac2d568d50f197d8c0239bb69cc9d1
iqbalthex <iqbalariem232@gmail.com>
2023-08-06 23:00:01 +0700

    deploy

--- src/components/ProfilePicture.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="cursor-pointer">
-    <img src="/images/ru.jpg" alt="pict" class="w-12 h-12 object-cover rounded-full" />
-  </div>
-</template>
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import './assets/css/app.css';
-
-import { createApp } from 'vue';
-import App from './App.vue';
-
-createApp(App).mount('#app');
