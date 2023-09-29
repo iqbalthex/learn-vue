967da6c9cef8ae9464b7dad3a89425e78af832de
iqbalthex <iqbalariem232@gmail.com>
2023-09-30 00:15:24 +0700

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,19 +2,29 @@
 
 import { ref } from 'vue';
 
+const navbarHidden = ref(false);
 const activeLink = ref('home');
 
 function changeActive(id) {
   activeLink.value = id;
 }
 
+function toggleNavbar() {
+  navbarHidden.value = !navbarHidden.value;
+}
+
 </script>
 
 <template>
 
-<header class="w-full px-4 flex justify-stretch lg:pr-6 bg-violet-400 lg:justify-end">
-  <nav class="hidden w-full py-3 lg:w-auto">
-    <ul class="block lg:flex gap-4">
+<header class="w-full px-4 py-3 lg:flex justify-stretch lg:pr-6 bg-violet-400 lg:justify-end">
+  <button @click="toggleNavbar" type="button" class="inline-flex p-2 text-gray-500 rounded-md lg:hidden hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" /></svg>
+  </button>
+
+  <nav :class="[ navbarHidden ? 'h-0' : 'h-full' ]"
+    class="w-full overflow-hidden lg:w-auto lg:h-full">
+    <ul class="gap-4 mt-4 lg:flex lg:mt-0">
       <li>
         <a :class="{ active: activeLink === 'home' }"
           @click="changeActive('home')"
